223a0835e24b9ff39c02028caf07cc0ea8cbb534
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-08 08:37:02 -0300

    Update AbstractFunctions.php

--- src/Functions/AbstractFunctions.php
+++ src/Functions/AbstractFunctions.php
@@ -4,9 +4,10 @@
  * This file is part of the UCSDMath package.
  *
  * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
+ * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ * @author Daryl Eisner <deisner@ucsd.edu>
  */
 
 declare(strict_types=1);
