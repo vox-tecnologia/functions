6c161e2710be08205ec723a94ee058a9f0bc2ada
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 14:03:45 -0300

    Update ServiceFunctions.php

--- src/Functions/ServiceFunctions.php
+++ src/Functions/ServiceFunctions.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Functions;
@@ -51,7 +41,6 @@ namespace UCSDMath\Functions;
  * ServiceFunctions provides a common set of implementations where needed. The ServiceFunctions
  * trait and the ServiceFunctionsInterface should be paired together.
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  */
 trait ServiceFunctions
 {
