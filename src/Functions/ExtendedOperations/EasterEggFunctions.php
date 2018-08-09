7313145e81561c8acc70ae29977f14038c4746cc
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 14:05:25 -0300

    Update EasterEggFunctions.php

--- src/Functions/ExtendedOperations/EasterEggFunctions.php
+++ src/Functions/ExtendedOperations/EasterEggFunctions.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Functions\ExtendedOperations;
@@ -31,7 +21,6 @@ namespace UCSDMath\Functions\ExtendedOperations;
  * EasterEggFunctions provides a common set of implementations where needed. The EasterEggFunctions
  * trait and the EasterEggFunctionsInterface should be paired together.
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  */
 trait EasterEggFunctions
 {
