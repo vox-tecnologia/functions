2d18c9cf98a0290df3fb0906be18248a1bdbe551
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 14:03:09 -0300

    Update Functions.php

--- src/Functions/Functions.php
+++ src/Functions/Functions.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Functions;
@@ -26,7 +16,6 @@ namespace UCSDMath\Functions;
  * (+) FunctionsInterface __construct();
  * (+) void __destruct();
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  */
 class Functions extends AbstractFunctions implements FunctionsInterface
 {
