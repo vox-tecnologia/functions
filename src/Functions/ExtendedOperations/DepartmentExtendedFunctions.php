114ec043266e12f67bc6fba2a25d31467b1e3b8e
Philipe Fernandes <phlllpe@users.noreply.github.com>
2018-08-09 14:04:31 -0300

    Update DepartmentExtendedFunctions.php

--- src/Functions/ExtendedOperations/DepartmentExtendedFunctions.php
+++ src/Functions/ExtendedOperations/DepartmentExtendedFunctions.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * This file is part of the UCSDMath package.
- *
- * (c) 2015-2017 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- * @author Daryl Eisner <deisner@ucsd.edu>
- */
-
 declare(strict_types=1);
 
 namespace UCSDMath\Functions\ExtendedOperations;
@@ -38,7 +28,6 @@ namespace UCSDMath\Functions\ExtendedOperations;
  * DepartmentExtendedFunctions provides a common set of implementations where needed. The DepartmentExtendedFunctions
  * trait and the DepartmentExtendedFunctionsInterface should be paired together.
  *
- * @author Daryl Eisner <deisner@ucsd.edu>
  */
 trait DepartmentExtendedFunctions
 {
